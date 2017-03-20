d194a5418805baf3bfb4cca540b6174b21cb0500
young1029 <564296982@qq.com>
2017-03-20 10:06:19 +0800

    fxy

--- src/main/java/com/bsth/SpringBootWebApplication.java
+++ src/main/java/com/bsth/SpringBootWebApplication.java
@@ -1,12 +1,26 @@
 package com.bsth;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class SpringBootWebApplication {
 
+	@Value("${book.name}")
+	private String name;
+	@Value("${book.auther}")
+	private String auther;
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootWebApplication.class, args);
+	}
+
+	@RequestMapping(value = "/")
+	String index(){
+		return "你好，世界！Hello World！"+"--"+auther+"写了"+name;
 	}
 }
